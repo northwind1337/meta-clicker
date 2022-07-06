266283983d6edcea2333ec2654bd11871a84a4b6
northwind1337 <brytong@ukr.net>
2022-07-06 13:52:47 +0300

    Refactoring

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
-const express = require('express');
+const express = require("express");
+
 const app = express();
 const serverPort = 9000;
 
-app.use('/clicker', express.static('web-app'));
+app.use("/clicker", express.static("web-app"));
 
-app.listen(serverPort, ()=>{
-    console.log(`Meta Clicker is listening to http://localhost:${serverPort}/clicker`);
-})
-
+app.listen(serverPort, () => {
+  console.log(
+    `Meta Clicker is listening to http://localhost:${serverPort}/clicker`
+  );
+});
